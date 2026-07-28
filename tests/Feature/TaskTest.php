1e4973cbc1fbb014f656103dd5b1aadbb01de2e3
Agdrael <rodrigojoserivera2016@gmail.com>
2026-07-28 15:45:34 -0600

    reduciendo codigo: susituir pruebas aindividuales por un dataset

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -6,67 +6,28 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-test('una tarea puede ser creada con una categoría válida', function () {
-    // Arrange
-    $category = Category::factory()->create();
-
+test('una tarea no puede ser creada con una categoria invalida',function(?int $categoryId){
     $data = [
-        'category_id' => $category->id,
-        'title' => 'Aprender pruebas en Laravel',
-        'description' => 'Probar la creación de tareas',
-        'completed' => false,
-        'due_date' => '2026-08-15',
-    ];
-
-    // Act
-    $response = $this->post(route('tasks.store'), $data);
-
-    // Assert
-    $response->assertSessionHasNoErrors();
-
-    $this->assertDatabaseHas('tasks', [
-        'category_id' => $category->id,
-        'title' => 'Aprender pruebas en Laravel',
-        'description' => 'Probar la creación de tareas',
-    ]);
-
-    $task = Task::query()
-        ->with('category')
-        ->where('title', 'Aprender pruebas en Laravel')
-        ->firstOrFail();
-
-    $this->assertFalse($task->completed);
-
-    $this->assertSame(
-        '2026-08-15',
-        $task->due_date->toDateString()
-    );
-
-    $this->assertTrue(
-        $task->category->is($category)
-    );
-
-    $response->assertRedirect(route('tasks.index'));
-});
-
-test('una tarea no puede ser creada con una categoria inexistente',function(){
-    $data =[
-        'category_id'=>99999,
+        'category_id'=>$categoryId,
         'title'=>'Tarea con categoria invalida',
         'description'=>'Esta tarea no debe guardarse',
         'completed'=>false,
-        'due_date'=>'2025-08-15'
+        'due_date'=>now()->addMonth()->toDateString(),
     ];
 
     $response = $this
         ->from(route('tasks.create'))
         ->post(route('tasks.store'),$data);
 
-    $response->assertSessionHasErrors('category_id')
-        ->assertRedirect(route('tasks.create'));
+    $response
+        ->assertOnlyInvalid(['category_id'])
+        ->assertRedirectToRoute('tasks.create');
 
-    $this->assertDataBaseCount('tasks',0);
-});
+    $this->assertDatabaseCount('tasks',0);
+})->with([
+    'la categoria es obligatoria'=>[null],
+    'la categoria no existe'=>[99999]
+]);
 
 test('una tarea no puede ser creada sin una categoria',function(){
     $data =[
@@ -83,6 +44,6 @@ test('una tarea no puede ser creada sin una categoria',function(){
      $response->assertOnlyInvalid(['category_id'])
         ->assertRedirectToRoute('tasks.create');
     
-    $this->assertDataBaseCount('tasks',0);
+    $this->assertDatabaseCount('tasks',0);
     
 });
